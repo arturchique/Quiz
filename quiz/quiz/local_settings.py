ec5f6d867723882479fcaedf8d2a957cece5de27
Artur <aepremyan3993@gmail.com>
2020-08-04 18:12:05 +0300

    Made a registration

---
+++ quiz/quiz/local_settings.py
@@ -0,0 +1,18 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$+x-4c0po-u16r&6&7p#565i#*xjgrlp@lxe-=0c&&chwiriof'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
---
+++ quiz/quiz/prod_settings.py
@@ -0,0 +1,21 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = '$+x-4c045-u16r&6&7p#565i#*xjgrlp@lxe-=0c&&chwiriof'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
